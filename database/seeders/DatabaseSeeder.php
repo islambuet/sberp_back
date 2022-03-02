faa9770aa0016de3192fc568072ae69bd4b2f2f3
Md. Shaiful Islam <islambuet@gmail.com>
2022-03-03 04:08:15 +0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
              UserGroupsSeeder::class,
             SystemConfigurationsSeeder::class,
             SystemTasksSeeder::class,                                    
-            // Occupations::class,                                    
-            // Users::class,            
+            OccupationsSeeder::class,                                    
+            UsersSeeder::class,            
         ]);
     }
 }
