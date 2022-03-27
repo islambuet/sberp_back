2f389a05aa0462ffa282e0f6c31c69378451ae09
Md. Shaiful Islam <islambuet@gmail.com>
2022-03-28 02:57:40 +0600

    Company UserController save done

--- app/Http/Controllers/system/setup/business/CompanyUsersController.php
+++ app/Http/Controllers/system/setup/business/CompanyUsersController.php
@@ -166,19 +166,10 @@ class CompanyUsersController extends RootController
         $query->select('company_users.*','users.id as user_id');
         $query->where('users.status',SYSTEM_STATUS_ACTIVE);
         $query->whereIn('users.id',$user_ids);
-        $query->leftJoin(TABLE_COMPANY_USERS.' as company_users' , 'company_users.user_id', '=', 'users.id');
-        
-
-        //     $query->orderBy('companies.id', 'DESC');
-        //     $query->orderBy('user_groups.id', 'DESC');
-
-            // $query->join(TABLE_COMPANIES.' as companies' , 'user_groups.company_id', '=', 'companies.id');
-            
-            // $query->where('user_groups.status','!=',SYSTEM_STATUS_DELETE);            
-            // $query->where('companies.status','!=',SYSTEM_STATUS_DELETE);    
-            // if($company_id>0){
-            //     $query->where('companies.id',$company_id);    
-            // }        
+        $query->leftJoin(TABLE_COMPANY_USERS.' as company_users' ,function($join) use($company_id) {
+            $join->on('users.id','=','company_users.user_id');
+            $join->on('company_users.company_id','=',DB::raw($company_id));
+        });        
         $results=$query->get();
         $user_infos=array();
         foreach ($results as $result){
@@ -213,129 +204,54 @@ class CompanyUsersController extends RootController
                 }
             }
         }
+        if($changed){
+            DB::beginTransaction();
+            foreach ($items as $index=>$item){
+                if(!$items[$index]['error'])
+                {
+                    try{
+                        $itemNew=$item;
+                        unset($itemNew['company_brach_id'],$itemNew['error'],$itemNew['messages']);
+                        $itemNew['company_id']=$company_id;
+                        $itemNew['company_brach_ids']=','.implode(',',$item['company_brach_id']).',';
+                        $itemNew['reason_status_active']=COMPANY_USER_STATUS_ACTIVE_ADMIN_ADD;
+                        $itemNew['status_active_id']=0;
+                        $itemNew['status']=SYSTEM_STATUS_ACTIVE;                        
+                        $itemNew['updated_by']=$this->user['id'];
+                        $itemNew['updated_at']=Carbon::now();
 
-
-        // echo '<pre>';
-        // print_r($user_infos);
-        // echo '</pre>';
-        // die();
-
-        //valdation userids except superadmin and admin ug
-
-        echo '<pre>';
-        // print_r($company_user_group_ids);
-        // print_r($company_brach_ids);
-        print_r($changed);
-        print_r($items);
-        echo '</pre>';
-        die();
-
-        die();
-        $itemId=$request->id?$request->id:0;
-
-        
-        $validation_rule=array();    
-        $validation_rule['name']=['required', 'string','min:3', 'max:255']; 
-        $validation_rule['company_id']=['required', 'numeric']; 
-        $validation_rule['ordering']=['numeric']; 
-        $validation_rule['status']=[Rule::in([SYSTEM_STATUS_ACTIVE, SYSTEM_STATUS_INACTIVE])]; 
-
-        $itemNew=$request->item;
-        $this->validateInputKeys($itemNew,array_keys($validation_rule));
-        
-        
-
-        if($itemId>0) {
-            if($this->permissions['action_2']!=1) {
-                return response()->json(['error'=>'ACCESS_DENIED','messages'=>__('messages.ACCESS_DENIED_EDIT')]);
-            }        
-            $result = DB::table(TABLE_COMPANY_USER_GROUPS)->select(array_keys($validation_rule))->find($itemId);       
-            if(!$result){
-                return response()->json(['error'=>'ITEM_NOT_FOUND','messages'=>__('validation.data_not_found',['attribute'=>'id: '.$itemId])]);
-            }
-            $itemOld=$result;
-            foreach($itemOld as $key=>$oldValue){
-                if($key=='company_id'){
-                    if(array_key_exists($key,$itemNew)){
-                        if($itemNew[$key]!=$oldValue){
-                            return response()->json(['error'=>'VALIDATION_FAILED','messages'=>__('Company Cannot be Changed')]);
+                        if($user_infos[$item['user_id']]->id>0){
+                            // $itemNew['updated_by']=$this->user['id'];
+                            // $itemNew['updated_at']=Carbon::now();
+                            // DB::table(TABLE_COMPANY_USER_GROUPS)->where('id',$itemId)->update($itemNew);                
+                            // unset($itemNew['updated_by'],$itemNew['updated_at']);
+                            
+                            // $dataHistory=array();
+                            // $dataHistory['table_name']=TABLE_COMPANY_USER_GROUPS;
+                            // $dataHistory['controller']=(new \ReflectionClass(__CLASS__))->getShortName();
+                            // $dataHistory['method']=__FUNCTION__;
+                            // $dataHistory['table_id']=$itemId;
+                            // $dataHistory['action']=DB_ACTION_EDIT;
+                            // $dataHistory['data_old']=json_encode($itemOld);
+                            // $dataHistory['data_new']=json_encode($itemNew);
+                            // $dataHistory['created_at']=Carbon::now();
+                            // $dataHistory['created_by']=$this->user['id'];
+                            // $this->dBSaveHistory($dataHistory,TABLE_SYSTEM_HISTORIES);
+                        } else {
+                            DB::table(TABLE_COMPANY_USERS)->insertGetId($itemNew);                
                         }
+                        //return response()->json(['error' => '','messages'=>__('UserGroup Saved Successfully'),'data' =>$itemNew]);            
+                    } catch (\Exception $ex) {  
+                                   
+                        DB::rollback();
+                        return response()->json(['error' => 'SERVER_ERROR', 'messages'=>__('messages.SERVER_ERROR')]);
                     }
-                    else{
-                        $itemNew[$key]=$oldValue;
-                    }                    
-                }
-                else if(array_key_exists($key,$itemNew)){
-                    if($itemOld->$key==$itemNew[$key]){
-                        unset($itemNew[$key]);
-                        unset($itemOld->$key);
-                        unset($validation_rule[$key]);
-                    } 
-                }
-                else{
-                    unset($validation_rule[$key]);
-                    unset($itemOld->$key); //no change
                 }
             }
-            if(!(count($itemNew)>1)){
-                return response()->json(['error'=>'VALIDATION_FAILED','messages'=>__('validation.input_not_changed')]);
-            }
-            
-        } 
-        else{
-            if($this->permissions['action_1']!=1) {
-                return response()->json(['error'=>'ACCESS_DENIED','messages'=>__('messages.ACCESS_DENIED_ADD')]);
-            }  
-        }
-        $this->validateInputValues($itemNew,$validation_rule);       
-        if(array_key_exists('name',$itemNew)){            
-            //no need !itemId because if name same already unset
-            $result = DB::table(TABLE_COMPANY_USER_GROUPS)->where('name', $itemNew['name'])->where('company_id',$itemNew['company_id'])->first();
-            if($result){
-                return response()->json(['error'=>'VALIDATION_FAILED','messages'=>__('validation.data_already_exists',['attribute'=>'name'])]);
-            }
-        }     
-        if(array_key_exists('company_id',$itemNew)){            
-            //no need !itemId because if name same already unset
-            $result = DB::table(TABLE_COMPANIES)->where('id',$itemNew['company_id'])->first();
-            if(!$result){
-                return response()->json(['error'=>'VALIDATION_FAILED','messages'=>__('Invalid Company')]);
-            }
-        }   
-        DB::beginTransaction();
-        try{
-            if($itemId>0){
-                $itemNew['updated_by']=$this->user['id'];
-                $itemNew['updated_at']=Carbon::now();
-                DB::table(TABLE_COMPANY_USER_GROUPS)->where('id',$itemId)->update($itemNew);                
-                unset($itemNew['updated_by'],$itemNew['updated_at']);
-                
-                $dataHistory=array();
-                $dataHistory['table_name']=TABLE_COMPANY_USER_GROUPS;
-                $dataHistory['controller']=(new \ReflectionClass(__CLASS__))->getShortName();
-                $dataHistory['method']=__FUNCTION__;
-                $dataHistory['table_id']=$itemId;
-                $dataHistory['action']=DB_ACTION_EDIT;
-                $dataHistory['data_old']=json_encode($itemOld);
-                $dataHistory['data_new']=json_encode($itemNew);
-                $dataHistory['created_at']=Carbon::now();
-                $dataHistory['created_by']=$this->user['id'];
-                $this->dBSaveHistory($dataHistory,TABLE_SYSTEM_HISTORIES);
-            } else {
-                $itemNew['created_by']=$this->user['id'];
-                $itemNew['created_at']=Carbon::now();
-                $itemNew['id'] = DB::table(TABLE_COMPANY_USER_GROUPS)->insertGetId($itemNew);                
-                unset($itemNew['created_by'],$itemNew['created_at']);
-            }            
             TokenHelper::updateSaveToken($save_token);
             DB::commit();
-            return response()->json(['error' => '','messages'=>__('UserGroup Saved Successfully'),'data' =>$itemNew]);            
-        } catch (\Exception $ex) {
-            print_r($ex);
-            // ELSE rollback & throw exception
-            DB::rollback();
-            return response()->json(['error' => 'SERVER_ERROR', 'messages'=>__('messages.SERVER_ERROR')]);
         }
+        return $items;
     }
 }
 
