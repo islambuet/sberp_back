37dbc7e4b079710063259a5ffe19d239814adffb
Md. Shaiful Islam <islambuet@gmail.com>
2022-04-07 02:59:41 +0600

    UserGroup Completed without save Role

--- app/Http/Controllers/company/setup/api.route.php
+++ app/Http/Controllers/company/setup/api.route.php
@@ -16,21 +16,11 @@ Route::middleware('logged-user')->group(function(){
     Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
     
 
-    $url='setup/business/company-user-groups';
-    $controllerClass=Controllers\system\setup\business\CompanyUserGroupsController::class;
+    $url='{companyId}/setup/company-user-groups';
+    $controllerClass=Controllers\company\setup\CompanyUserGroupsController::class;
 
     Route::match(array('GET','POST'),$url.'/initialize', [$controllerClass, 'initialize']);
     Route::match(array('GET','POST'),$url.'/get-items', [$controllerClass, 'getItems']);
     Route::match(array('GET','POST'),$url.'/get-item/{itemId}', [$controllerClass, 'getItem']);
     Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
-
-
-    $url='setup/business/company-users';
-    $controllerClass=Controllers\system\setup\business\CompanyUsersController::class;
-
-    Route::match(array('GET','POST'),$url.'/initialize', [$controllerClass, 'initialize']);
-    Route::match(array('GET','POST'),$url.'/get-items', [$controllerClass, 'getItems']);
-    // Route::match(array('GET','POST'),$url.'/get-item/{itemId}', [$controllerClass, 'getItem']);
-    Route::post($url.'/save-items', [$controllerClass, 'saveItems']);
-
 });
