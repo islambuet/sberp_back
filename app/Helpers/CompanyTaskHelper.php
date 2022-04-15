982d41df101882ba5d8bbeab16aba9510fe5ec3b
Md. Shaiful Islam <islambuet@gmail.com>
2022-04-16 03:19:09 +0600

    Fixed Max level problem

--- app/Helpers/CompanyTaskHelper.php
+++ app/Helpers/CompanyTaskHelper.php
@@ -1,122 +1,106 @@
 <?php
-    namespace App\Helpers;
-    use Illuminate\Support\Facades\DB;
-    use Illuminate\Support\Facades\Auth;
-    class CompanyTaskHelper
+namespace App\Helpers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+class CompanyTaskHelper
+{
+    public static $MAX_MODULE_ACTIONS = 4;
+    public static function getCompanyUserGroupRoleByUser($user_id)
     {
-        public static $MAX_MODULE_ACTIONS=4;
-        public static function getCompanyUserGroupRoleByUser($user_id)
-        {
-            $query=DB::table(TABLE_COMPANY_USERS.' as company_users');
-            $query->where('company_users.status','=',SYSTEM_STATUS_ACTIVE);    
-            $query->where('company_users.user_id','=',$user_id);  
-            $query->select('company_users.company_id');
-            $query->addselect('company_users.company_user_group_id');
-            
-            $query->join(TABLE_COMPANY_USER_GROUPS.' as user_groups' , 'company_users.company_user_group_id', '=', 'user_groups.id');
-            for($i=0;$i<self::$MAX_MODULE_ACTIONS;$i++)
-            {
-                $query->addselect('user_groups.action_'.$i);
-                //$role['action_'.$i]=',';
-            }
+        $query = DB::table(TABLE_COMPANY_USERS . ' as company_users');
+        $query->where('company_users.status', '=', SYSTEM_STATUS_ACTIVE);
+        $query->where('company_users.user_id', '=', $user_id);
+        $query->select('company_users.company_id');
+        $query->addselect('company_users.company_user_group_id');
 
-            $results=$query->get();
-            $role=array();
-            foreach ($results as $result){
-                $role[$result->company_id]=$result;                
-            }
-            return $role;
+        $query->join(TABLE_COMPANY_USER_GROUPS . ' as user_groups', 'company_users.company_user_group_id', '=', 'user_groups.id');
+        for ($i = 0; $i < self::$MAX_MODULE_ACTIONS; $i++) {
+            $query->addselect('user_groups.action_' . $i);
+            //$role['action_'.$i]=',';
         }
-        public static function getPermissions($companyId,$url,$companyUserGroupRole)//forApi
-        {
-            $permissions = array();
-            $task=DB::table(TABLE_COMPANY_TASKS)->where('url', $url)->select('id')->first();
-            $taskId=$task?$task->id:0;
-            for($i=0; $i<self::$MAX_MODULE_ACTIONS; $i++)
-            {
-                //if(strpos($userGroupRole['action_'.$i], ','.$taskId.',')!==false){
-                if(isset($companyUserGroupRole[$companyId])&& (strpos($companyUserGroupRole[$companyId]->{'action_'.$i}, ','.$taskId.',')!==false)){
-                    $permissions['action_'.$i] = 1;
-                }
-                else
-                {
-                    $permissions['action_'.$i]=0;
-                }
-            }
-            return $permissions;
-        }
-        public static function getCompanyUserGroupMenu($companyId,$companyUserGroupRole)
-        {
-            $tree=array();
-            $max_level=1;
-            if(isset($companyUserGroupRole[$companyId])){
-                $userGroupRole=$companyUserGroupRole[$companyId];
-                
-                $role=array();
-                if(strlen($userGroupRole->action_0)>1)
-                {
-                    $role=explode(',',trim($userGroupRole->action_0,','));
-                }
 
-                $tasks=DB::table(TABLE_COMPANY_TASKS)
-                    ->select('id','name','type','parent','type','url','ordering','status')
-                    ->orderBy('ordering', 'ASC')
-                    ->where('status',SYSTEM_STATUS_ACTIVE)
-                    ->get()->toArray();
-                $children=array();
-                foreach($tasks as $task)
-                {
-                    $task=(array)$task;
-                    if($task['type']=='TASK')
-                    {
-                        if(in_array($task['id'],$role))
-                        {
-                            $children[$task['parent']][$task['id']]=$task;
-                        }
-                    }
-                    else
-                    {
-                        $children[$task['parent']][$task['id']]=$task;
-                    }
-                }
-                if(isset($children[0]))
-                {
-                    $tree = self::getCompanyUserGroupSubMenu(1,$max_level,'','',$children, $children[0]);
-                }                
-            }            
-            return array('max_level'=>$max_level,'menu'=>$tree);
+        $results = $query->get();
+        $role = [];
+        foreach ($results as $result) {
+            $role[$result->company_id] = $result;
         }
-        public static function getCompanyUserGroupSubMenu($level,&$max_level,$parent_class,$prefix,$list, $parent)
-        {
-            if($level>$max_level)
-            {
-                $max_level=$level;
-            }
-            $tree = array();
-            foreach ($parent as $key=>$element)
-            {
-                $element['level']=$level;
-                $element['parent_class']=$parent_class;
-                $element['prefix,']=$prefix;
-                //$tree[] = $element;
-                if(isset($list[$element['id']]))
-                {
-                    $children=self::getCompanyUserGroupSubMenu($level+1,$max_level,$parent_class.' parent_'.$element['id'],$prefix.'- ',$list, $list[$element['id']]);
-                    if($children)
-                    {
-                        $element['children'] = $children;
-                        $tree[] = $element;
-                    }
-                }
-                else
-                {
-                    if($element['type']=='TASK')
-                    {
-                        $tree[] = $element;
-                    }
-                }
-            }
-            return $tree;
-
-        }
+        return $role;
     }
+    public static function getPermissions($companyId, $url, $companyUserGroupRole) //forApi
+
+    {
+        $permissions = [];
+        $task = DB::table(TABLE_COMPANY_TASKS)->where('url', $url)->select('id')->first();
+        $taskId = $task ? $task->id : 0;
+        for ($i = 0; $i < self::$MAX_MODULE_ACTIONS; $i++) {
+            //if(strpos($userGroupRole['action_'.$i], ','.$taskId.',')!==false){
+            if (isset($companyUserGroupRole[$companyId]) && (strpos($companyUserGroupRole[$companyId]->{'action_' . $i}, ',' . $taskId . ',') !== false)) {
+                $permissions['action_' . $i] = 1;
+            } else {
+                $permissions['action_' . $i] = 0;
+            }
+        }
+        return $permissions;
+    }
+    public static function getCompanyUserGroupMenu($companyId, $companyUserGroupRole)
+    {
+        $tree = [];
+        $max_level = 0;
+        if (isset($companyUserGroupRole[$companyId])) {
+            $userGroupRole = $companyUserGroupRole[$companyId];
+
+            $role = [];
+            if (strlen($userGroupRole->action_0) > 1) {
+                $role = explode(',', trim($userGroupRole->action_0, ','));
+            }
+
+            $tasks = DB::table(TABLE_COMPANY_TASKS)
+                ->select('id', 'name', 'type', 'parent', 'type', 'url', 'ordering', 'status')
+                ->orderBy('ordering', 'ASC')
+                ->where('status', SYSTEM_STATUS_ACTIVE)
+                ->get()->toArray();
+            $children = [];
+            foreach ($tasks as $task) {
+                $task = (array) $task;
+                if ($task['type'] == 'TASK') {
+                    if (in_array($task['id'], $role)) {
+                        $children[$task['parent']][$task['id']] = $task;
+                    }
+                } else {
+                    $children[$task['parent']][$task['id']] = $task;
+                }
+            }
+            if (isset($children[0])) {
+                $tree = self::getCompanyUserGroupSubMenu(1, $max_level, '', '', $children, $children[0]);
+            }
+        }
+        return ['max_level' => $max_level, 'menu' => $tree];
+    }
+    public static function getCompanyUserGroupSubMenu($level, &$max_level, $parent_class, $prefix, $list, $parent)
+    {
+        $tree = [];
+        foreach ($parent as $key => $element) {
+            $element['level'] = $level;
+            $element['parent_class'] = $parent_class;
+            $element['prefix,'] = $prefix;
+            //$tree[] = $element;
+            if (isset($list[$element['id']])) {
+                $children = self::getCompanyUserGroupSubMenu($level + 1, $max_level, $parent_class . ' parent_' . $element['id'], $prefix . '- ', $list, $list[$element['id']]);
+                if ($children) {
+                    $element['children'] = $children;
+                    $tree[] = $element;
+                }
+            } else {
+                if ($element['type'] == 'TASK') {
+                    $tree[] = $element;
+                    if ($level > $max_level) {
+                        $max_level = $level;
+                    }
+                }
+            }
+        }
+        return $tree;
+
+    }
+}
--- app/Helpers/TaskHelper.php
+++ app/Helpers/TaskHelper.php
@@ -1,114 +1,98 @@
 <?php
-    namespace App\Helpers;
-    use Illuminate\Support\Facades\DB;
-    use Illuminate\Support\Facades\Auth;
-    class TaskHelper
+namespace App\Helpers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+class TaskHelper
+{
+    public static $MAX_MODULE_ACTIONS = 4;
+    public static function getUserGroupRole($user_group_id)
     {
-        public static $MAX_MODULE_ACTIONS=4;
-        public static function getUserGroupRole($user_group_id)
-        {
-            $role=array();
-            $query=DB::table(TABLE_USER_GROUPS);
-            $query->where('id', $user_group_id);
-            for($i=0;$i<self::$MAX_MODULE_ACTIONS;$i++)
-            {
-                $query->addselect('action_'.$i);
-                $role['action_'.$i]=',';
-            }
-            $userGroup=$query->first();
-            if($userGroup) {
-                for($i=0;$i<self::$MAX_MODULE_ACTIONS;$i++) {
-                    $role['action_'.$i]=$userGroup->{'action_'.$i};
-                }
-            }
-            return $role;
+        $role = [];
+        $query = DB::table(TABLE_USER_GROUPS);
+        $query->where('id', $user_group_id);
+        for ($i = 0; $i < self::$MAX_MODULE_ACTIONS; $i++) {
+            $query->addselect('action_' . $i);
+            $role['action_' . $i] = ',';
         }
-        public static function getPermissions($url,$userGroupRole)//forApi
-        {
-            $permissions = array();
-            $task=DB::table(TABLE_SYSTEM_TASKS)->where('url', $url)->select('id')->first();
-            $taskId=$task?$task->id:0;
-            for($i=0; $i<self::$MAX_MODULE_ACTIONS; $i++)
-            {
-                if(strpos($userGroupRole['action_'.$i], ','.$taskId.',')!==false){
-                    $permissions['action_'.$i] = 1;
-                }
-                else
-                {
-                    $permissions['action_'.$i]=0;
-                }
+        $userGroup = $query->first();
+        if ($userGroup) {
+            for ($i = 0; $i < self::$MAX_MODULE_ACTIONS; $i++) {
+                $role['action_' . $i] = $userGroup->{'action_' . $i};
             }
-            return $permissions;
         }
-        public static function getUserGroupMenu($userGroupRole)
-        {
-            $role=array();
-            if(strlen($userGroupRole['action_0'])>1)
-            {
-                $role=explode(',',trim($userGroupRole['action_0'],','));
-            }
-
-            $tasks=DB::table(TABLE_SYSTEM_TASKS)
-                ->select('id','name','type','parent','type','url','ordering','status')
-                ->orderBy('ordering', 'ASC')
-                ->where('status',SYSTEM_STATUS_ACTIVE)
-                ->get()->toArray();
-            $children=array();
-            foreach($tasks as $task)
-            {
-                $task=(array)$task;
-                if($task['type']=='TASK')
-                {
-                    if(in_array($task['id'],$role))
-                    {
-                        $children[$task['parent']][$task['id']]=$task;
-                    }
-                }
-                else
-                {
-                    $children[$task['parent']][$task['id']]=$task;
-                }
-            }
-            $tree=array();
-            $max_level=1;
-            
-            if(isset($children[0]))
-            {
-                $tree = self::getUserGroupSubMenu(1,$max_level,'','',$children, $children[0]);
-            }
-            return array('max_level'=>$max_level,'menu'=>$tree);
-        }
-        public static function getUserGroupSubMenu($level,&$max_level,$parent_class,$prefix,$list, $parent)
-        {
-            if($level>$max_level)
-            {
-                $max_level=$level;
-            }
-            $tree = array();
-            foreach ($parent as $key=>$element)
-            {
-                $element['level']=$level;
-                $element['parent_class']=$parent_class;
-                $element['prefix,']=$prefix;
-                //$tree[] = $element;
-                if(isset($list[$element['id']]))
-                {
-                    $children=self::getUserGroupSubMenu($level+1,$max_level,$parent_class.' parent_'.$element['id'],$prefix.'- ',$list, $list[$element['id']]);
-                    if($children)
-                    {
-                        $element['children'] = $children;
-                        $tree[] = $element;
-                    }
-                }
-                else
-                {
-                    if($element['type']=='TASK')
-                    {
-                        $tree[] = $element;
-                    }
-                }
-            }
-            return $tree;
-
-        }
+        return $role;
     }
+    public static function getPermissions($url, $userGroupRole) //forApi
+
+    {
+        $permissions = [];
+        $task = DB::table(TABLE_SYSTEM_TASKS)->where('url', $url)->select('id')->first();
+        $taskId = $task ? $task->id : 0;
+        for ($i = 0; $i < self::$MAX_MODULE_ACTIONS; $i++) {
+            if (strpos($userGroupRole['action_' . $i], ',' . $taskId . ',') !== false) {
+                $permissions['action_' . $i] = 1;
+            } else {
+                $permissions['action_' . $i] = 0;
+            }
+        }
+        return $permissions;
+    }
+    public static function getUserGroupMenu($userGroupRole)
+    {
+        $role = [];
+        if (strlen($userGroupRole['action_0']) > 1) {
+            $role = explode(',', trim($userGroupRole['action_0'], ','));
+        }
+
+        $tasks = DB::table(TABLE_SYSTEM_TASKS)
+            ->select('id', 'name', 'type', 'parent', 'type', 'url', 'ordering', 'status')
+            ->orderBy('ordering', 'ASC')
+            ->where('status', SYSTEM_STATUS_ACTIVE)
+            ->get()->toArray();
+        $children = [];
+        foreach ($tasks as $task) {
+            $task = (array) $task;
+            if ($task['type'] == 'TASK') {
+                if (in_array($task['id'], $role)) {
+                    $children[$task['parent']][$task['id']] = $task;
+                }
+            } else {
+                $children[$task['parent']][$task['id']] = $task;
+            }
+        }
+        $tree = [];
+        $max_level = 0;
+
+        if (isset($children[0])) {
+            $tree = self::getUserGroupSubMenu(1, $max_level, '', '', $children, $children[0]);
+        }
+        return ['max_level' => $max_level, 'menu' => $tree];
+    }
+    public static function getUserGroupSubMenu($level, &$max_level, $parent_class, $prefix, $list, $parent)
+    {
+        $tree = [];
+        foreach ($parent as $key => $element) {
+            $element['level'] = $level;
+            $element['parent_class'] = $parent_class;
+            $element['prefix,'] = $prefix;
+            //$tree[] = $element;
+            if (isset($list[$element['id']])) {
+                $children = self::getUserGroupSubMenu($level + 1, $max_level, $parent_class . ' parent_' . $element['id'], $prefix . '- ', $list, $list[$element['id']]);
+                if ($children) {
+                    $element['children'] = $children;
+                    $tree[] = $element;
+                }
+            } else {
+                if ($element['type'] == 'TASK') {
+                    $tree[] = $element;
+                    if ($level > $max_level) {
+                        $max_level = $level;
+                    }
+                }
+            }
+        }
+        return $tree;
+
+    }
+}
